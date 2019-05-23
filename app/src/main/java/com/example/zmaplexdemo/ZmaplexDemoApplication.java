95a23ce0a87e98d50cb987df0498479898878cb6
Zmaplex <Zmaplex@gmail.com>
2019-05-23 11:01:12 +0800

    初始化项目

---
+++ app/src/main/java/com/example/zmaplexdemo/ZmaplexDemoApplication.java
@@ -0,0 +1,11 @@
+package com.example.zmaplexdemo;
+
+import android.app.Application;
+
+public class ZmaplexDemoApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
